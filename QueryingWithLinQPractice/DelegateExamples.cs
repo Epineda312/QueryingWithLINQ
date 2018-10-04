48f561e321eef63efb877d45814c087f31ba63af
Airic <epineda773@protonmail.com>
2018-10-04 13:01:24 -0700

    Update DelegateExamples.cs

--- QueryingWithLinQPractice/DelegateExamples.cs
+++ QueryingWithLinQPractice/DelegateExamples.cs
@@ -3,22 +3,23 @@ using System;
 namespace FunctionalProgramming
 {
     class Program
-    {
-        delegate void SayGreeting(string name);
-      
-        public static void SayHello(string name)
-        {
-          Console.WriteLine(string.Format("Later, {0}", name));
-        }
-      
+    {                  
         static void Main(string[] args)
         {
-          SayGreeting sayGreeting = new SayGreeting(SayHello);
+          Action<string> sayGreeting;
+          SayGreeting sayGreeting = delegate(string name)
+          {
+             Console.WriteLine(string.Format("Hello, {0}", name));
+          };
+          
           Console.WriteLine("What's your name?");
           string input = Console.ReadLine();
           sayGreeting(input);
           Console.ReadLine();
-          sayGreeting = new SayGreeting(SayGoodbye);
+          sayGreeting = delegate(string name)
+          {
+             Console.WriteLine(string.Format("Later, {0}", name));
+          };
           sayGreeting(input);
         }
     }
